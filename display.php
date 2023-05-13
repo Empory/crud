0d831a0b3b1651873be94c3ab51999891ab3c8ad
Empory ~ <oyuneverytime@gmail.com>
2023-05-13 14:57:45 +0300

    .

--- display.php
+++ display.php
@@ -11,67 +11,67 @@ include 'connect.php'
 </head>
 <body>
    <div class="container">
-    <button class="btn btn-primary my-5">
-        <a class="text-light" href="user.php">Add user</a>
-    </button>
-    <table class="table">
-        <thead>
-            <tr>
-                <th scope="col">Sl No</th>
-                <th scope="col">Name</th>
-                <th scope="col">Email</th>
-                <th scope="col">Mobile</th>
-                <th scope="col">Password</th>
-                <th scope="col">Operations</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php
-                $sql = "SELECT * FROM CRUD";
-                $result= mysqli_query($con, $sql);
-                if ($result) {
-                    while($row=mysqli_fetch_assoc($result)){
+        <button class="btn btn-primary my-5">
+            <a class="text-light" href="user.php">Add user</a>
+        </button>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">Sl No</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Mobile</th>
+                    <th scope="col">Password</th>
+                    <th scope="col">Operations</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                    $sql = "SELECT * FROM CRUD";
+                    $result= mysqli_query($con, $sql);
+                    if ($result) {
+                        while($row=mysqli_fetch_assoc($result)){
 
-                      $id= $row["id"];
-                      $name = $row["name"];
-                      $email = $row["email"];
-                      $mobile = $row["mobile"];
-                      $password = $row["password"];
-                      echo `<tr>
-                            <th scope="row">`.$id.`</th>
-                            <td>`.$name.`</td>
-                            <td>`.$email.`</td>
-                            <td>`.$mobile.`</td>
-                            <td>`.$password.`</td>
-                        </tr>`;
+                            $id= $row["id"];
+                            $name = $row["name"];
+                            $email = $row["email"];
+                            $mobile = $row["mobile"];
+                            $password = $row["password"];
+                            echo '<tr>
+                                    <th scope="row">'.$id.'</th>
+                                    <td>'.$name.'</td>
+                                    <td>'.$email.'</td>
+                                    <td>'.$mobile.'</td>
+                                    <td>'.$password.'</td>
+                                </tr>';
+                        }
+                            
                     }
-                        
-                }
-            ?>
-            <!-- <tr>
-                <th scope="row">1</th>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
-                <td>@mdo</td>
-            </tr>
-            <tr>
-                <th scope="row">2</th>
-                <td>Jacob</td>
-                <td>Thornton</td>
-                <td>@fat</td>
-                <td>@fat</td>
-            </tr>
-            <tr>
-                <th scope="row">3</th>
-                <td>Larry</td>
-                <td>the Bird</td>
-                <td>@twitter</td>
-                <td>@twitter</td>
-            </tr> -->
-        </tbody>
-        
+                ?>
+                <!-- <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                    <td>@mdo</td>
+                </tr>
+                <tr>
+                    <th scope="row">2</th>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                    <td>@fat</td>
+                </tr>
+                <tr>
+                    <th scope="row">3</th>
+                    <td>Larry</td>
+                    <td>the Bird</td>
+                    <td>@twitter</td>
+                    <td>@twitter</td>
+                </tr> -->
+            </tbody>
+            
+        </table>
     </div>
-</table>
 </body>
 </html>
